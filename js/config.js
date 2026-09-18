640ae60f9f7c9f4bf2e3411cce22bed812e440ec
behradb44-sketch <behradb44@gmail.com>
2026-09-18 08:06:38 -0700

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-export const CONFIG={
- BACKEND_HTTP: location.origin,
- BACKEND_WS: (location.protocol==="https:"?"wss:":"ws:")+"//"+location.host,
- MAX_PLAYERS:20,
- TICK_RATE:20,
- INPUT_RATE:20,
- WORLD:{w:3600,h:2400},
- VOICE:{stun:"stun:stun.l.google.com:19302"}
-};
